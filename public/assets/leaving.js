9de6c3b1d4b49793480347a40064ce87422fe56e
Kieran Coughlan <kcoughlan78@me.com>
2013-06-09 20:50:06 +0100

    precompile script.js

---
+++ public/assets/leaving.js
@@ -0,0 +1,1 @@
+document.getElementById("bowler").onclick=function(){return confirm("You are now leaving the Gallery of Photography, click OK to continue.")},document.getElementById("eoin").onclick=function(){return confirm("You are now leaving the Gallery of Photography, click OK to continue.")};
---
+++ public/assets/leaving1.js
@@ -0,0 +1,1 @@
+document.getElementById("bochum").onclick=function(){return confirm("You are now leaving the Gallery of Photography, click OK to continue.")};
