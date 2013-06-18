4d833df7323caa40451bcdf049916fb78d26d9e6
Kieran Coughlan <kcoughlan78@me.com>
2013-06-18 18:57:47 +0100

    added create controller

--- app/controllers/artlessons_controller.rb
+++ app/controllers/artlessons_controller.rb
@@ -10,4 +10,16 @@ class ArtlessonsController < ApplicationController
   def edit
     @artlesson = Artlesson.find(params[:id])
   end
+
+  def create
+    @artlesson = Artlesson.create(params[:artlesson])
+
+      respond_to do |format|
+        if @artlesson.save
+          format.html { redirect_to root_url, notice: 'Complaint successfully sent.' }
+        else
+          format.html { render action: "new" }
+        end
+      end
+  end
 end
